9ab2fca527a803ffb9b18c838047e345d9843e10
ude.ccoelho <ude.ccoelho@cas419298C14713.senacsp.edu.br>
2015-08-14 20:06:06 -0300

    Inclusão do Método Main

---
+++ src/main/java/br/senac/tads/pi3/exercicio01/exercicio01/App.java
@@ -0,0 +1,13 @@
+package br.senac.tads.pi3.exercicio01.exercicio01;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
