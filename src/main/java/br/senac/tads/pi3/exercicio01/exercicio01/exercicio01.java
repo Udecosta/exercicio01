4d98ef5c1929f51450ab37dd0f5f20346db069b0
bruno.clopes <bruno.clopes@CAS419198C14731.senacsp.edu.br>
2015-08-14 21:59:48 -0300

    Contato

--- src/main/java/br/senac/tads/pi3/exercicio01/exercicio01/exercicio01.java
+++ src/main/java/br/senac/tads/pi3/exercicio01/exercicio01/exercicio01.java
@@ -1,5 +1,7 @@
 package br.senac.tads.pi3.exercicio01.exercicio01;
 
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -14,7 +16,17 @@ public class exercicio01 {
     
     public static void main (String args []){
         
-        System.out.println("Hello World!");
+        Scanner leitor = new Scanner(System.in);
+        String nome, dataNasc, email, tel;
+        System.out.println("Contato:");
+        System.out.print("Nome: ");
+        nome = leitor.nextLine();
+        System.out.print("Data de Nascimento:");
+        dataNasc = leitor.next();
+        System.out.print("E-mail: ");
+        email = leitor.next();
+        System.out.println("Telefone: ");
+        tel = leitor.next();
     }
     
 }
