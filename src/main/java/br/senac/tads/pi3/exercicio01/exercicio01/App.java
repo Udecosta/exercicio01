ec1869343040d255be9db400f5dfe73444d3ab5f
ude.ccoelho <ude.ccoelho@CAS419300C14710.senacsp.edu.br>
2015-08-25 22:14:21 -0300

    Foi implementado a Função deletar o Cliente

--- src/main/java/br/senac/tads/pi3/exercicio01/exercicio01/App.java
+++ src/main/java/br/senac/tads/pi3/exercicio01/exercicio01/App.java
@@ -4,11 +4,9 @@ package br.senac.tads.pi3.exercicio01.exercicio01;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-    
-        
+public class App {
+
+    public static void main(String[] args) {
+
     }
 }
